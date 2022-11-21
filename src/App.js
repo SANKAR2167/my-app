cc73119fd22324348013dc92ac7ca4931998c10e
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-21 14:39:33 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,51 @@ import './App.css';
 
 // app component = view + logic (declaration)
 function App() {
+
+  const users=[
+    {name : "sankar",
+    url : ""},
+    {name : "santhosh",
+    url : ""},
+    {name : "Gowsi",
+    url : ""}];
+
+    const names=["Gowsalya", "Subbu", "Karthi"]
   return (
     <div className="App">
-      <User />
+      {/* <User 
+      name = "sankar"
+      url = ""/>
+      <User 
+      name = "santhosh"
+      url = ""/>
+      <User 
+      name = "Gowsi"
+      url = ""/> */}
+
+      {users.map((usr)=>(<User name={usr.name} url={usr.url}/>))}
     </div>
   );
 }
 
 // user component (declaration)
 
-function User() {
-  let name = "sankar";
+function User({url, name}) {
+
+  //const {pic, name}=props; //object destructuring in JS ES6
   return(
-      <h1>Hi, {name}</h1>
+    <section>
+      <img src={url} alt={name}/>
+      <h1>Hi, <span className='name_app'>{name}</span></h1>
+    </section> 
   );
+}
+
+function urs({name}){
+  return(
+    <section>
+      <h1>Hello, {name}</h1>
+    </section>
+  )
 }
 export default App;
