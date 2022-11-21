95d4043eaecde2c36df1cd56aecdeca330292526
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-21 11:32:35 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+// app component = view + logic (declaration)
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User />
     </div>
   );
 }
 
+// user component (declaration)
+
+function User() {
+  let name = "sankar";
+  return(
+      <h1>Hi, {name}</h1>
+  );
+}
 export default App;
