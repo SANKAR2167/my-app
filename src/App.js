d7a20df1880f55bb3c596b38bd6b5ffbd12c35c0
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-25 15:44:28 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import { MovieList } from './MovieList';
+import {Routes, Route, Link} from 'react-router-dom';
+
 // app component = view + logic (declaration)
 function App() {
   return (
     <div className="App">
-      <MovieList/>
+      {/* <MovieList/> */}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<MovieList/>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+function Home(){
+  return(
+    <div>
+      <h1>welcome to movie app</h1>
+    </div>
+  )
+}
