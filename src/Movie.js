b2ce2ca840c59dfceda4fe32efd9a6fb51ce0e81
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-25 11:00:26 +0530

    d

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,8 @@
 import { Counter } from './Counter';
 import { useState } from 'react';
-
-
+import IconButton from '@mui/material/IconButton';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 export function Movie({ movie }) {
   const styles = {
     color: movie.rating >= 8.5 ? 'green' : 'red'
@@ -15,7 +16,9 @@ export function Movie({ movie }) {
         <h2 className='movie-name'>{movie.name}</h2>
         <p className='movie-rating' style={styles}>⭐ {movie.rating}</p>
       </div>
-      <button onClick={() => setShow(!show)}>Toggle</button>
+      <IconButton onClick={() => setShow(!show)} aria-label="toggle">
+      {show ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+      </IconButton>
       {show ? <p className='movie-summary'>{movie.summary}</p> : null}
       <Counter />
     </div>
