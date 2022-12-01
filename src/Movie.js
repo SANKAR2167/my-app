e736a04a0e1f25fc8991ba70560c6da182e9cb23
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-12-01 07:17:23 +0530

    new change

--- src/Movie.js
+++ src/Movie.js
@@ -3,19 +3,27 @@ import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-export function Movie({ movie }) {
+import InfoIcon from '@mui/icons-material/Info';
+import { useNavigate } from 'react-router-dom';
+
+export function Movie({ movie, id}) {
   const styles = {
     color: movie.rating >= 8.5 ? 'green' : 'red'
   };
 
   const [show, setShow] = useState(true);
+  const navigate = useNavigate();
   return (
     <div className='movie-container'>
       <img src={movie.poster} alt={movie.name} className='movie-poster' />
       <div className='movie-specs'>
         <h2 className='movie-name'>{movie.name} <IconButton onClick={() => setShow(!show)} aria-label="toggle" color='primary'>
       {show ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
-      </IconButton></h2>
+      </IconButton> 
+      <IconButton aria-label="toggle" color='primary'
+      onClick={() => navigate(`/movies/${id}`)}>
+      <InfoIcon/>
+      </IconButton> </h2>
         <p className='movie-rating' style={styles}>⭐ {movie.rating}</p>
       </div>
       
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { Movie } from "./Movie";
 import { AddMovie } from "./AddMovie";
-export function MovieList({movieList, setMovieList}) {
+export function MovieList({movieList}) {
   return (
     <div>
       <div className='movie-list'>
-        {movieList.map((mv, index) => (<Movie key={index} movie={mv} />))}
+        {movieList.map((mv, index) => (<Movie key={index} movie={mv} id={index} />))}
       </div>
     </div>
   );
