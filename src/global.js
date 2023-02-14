28229fff5657da7d2b2fd9d614992cc49fd4f7d0
SANKAR2167 <snakarkulandaivel@gmail.com>
2023-02-14 10:25:11 +0530

    api modification

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-// export const API = 'https://638af1ba7220b45d22850b2c.mockapi.io';
+ export const API = 'https://638af1ba7220b45d22850b2c.mockapi.io';
 // export const API = "http://localhost:4000/"
-export const API = 'https://express-js-omega.vercel.app'
+// export const API = 'https://express-js-omega.vercel.app'
